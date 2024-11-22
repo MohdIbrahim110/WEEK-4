34c924f6c3502f6ae6274dc79a36c4a3c2fbdf43
MohdIbrahim110 <vikkyrizvi110@gmail.com>
2024-11-22 09:41:43 +0530

    Add files via upload

---
+++ ibrahim c++ week 4/prog2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main(){
+int num1, num2 ,i;
+cout<<"enter the first number ";
+cin>>num1;
+cout<<"enter the second number ";
+cin>>num2;
+for(i=0;i<num2;i++)
+{ 
+num1++;
+}
+cout<<"the sum of two nums is " <<num1;
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
